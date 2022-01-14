d079455af4dbcf25cd0e81dc9bc9124464074765
Ewelina <ewelina2502@gmail.com>
2022-01-14 15:19:15 +0100

    refactor code

---
+++ src/test/java/BookingTests.java
@@ -0,0 +1,294 @@
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import static io.restassured.RestAssured.*;
+
+public class BookingTests extends Data {
+
+    @Test
+    public void postBooking() {
+        String url = urlBooking();
+        String contentType = "application/json";
+
+        String body = printJsonBooking();
+
+        Response response = RestAssured.
+                given().
+                contentType(contentType).
+                body(body).
+                when().
+                post(url).
+                then().
+                extract().
+                response();
+
+        Assert.assertEquals(response.getStatusCode(),200);
+        System.out.println("Body :" + response.getBody().asString());
+    }
+
+    @Test
+    public void getBookingsReturns() {
+        Response response = get(urlBooking());
+
+//        System.out.println("Response :" + response.asString());
+        System.out.println("status code: " + response.getStatusCode());
+        System.out.println("Body :" + response.getBody().asString());
+
+        int statusCode = response.getStatusCode();
+        Assert.assertEquals(statusCode, 200);
+    }
+
+    @Test
+    public void getBookingsStatus() {
+        given().
+                get(urlBooking()).
+                then().
+                statusCode(200);
+    }
+
+    @Test
+    public void postAndPutBooking() {
+        String url = urlBooking();
+        String contentType = "application/json";
+        String bodyData = printJsonBooking();
+
+        Response response = RestAssured.
+                given().
+                contentType(contentType).
+                body(bodyData).
+                when().
+                post(url).
+                then().
+                extract().
+                response();
+
+        Assert.assertEquals(response.getStatusCode(), 200);
+        System.out.println("Body :" + response.getBody().asString());
+        int bookingid = response.jsonPath().getInt("bookingid");
+        System.out.println("bookingid: "  + bookingid);
+
+        String put_url = urlBooking() + "/"+ bookingid;
+        String cookies = cookies();
+        String authorization = authorization();
+        String bodyDataPut = printJsonPutBooking();
+
+        Response response_put = RestAssured.
+                given().
+                contentType(contentType).
+                body(bodyDataPut).
+                cookie(cookies).
+                header("Authorization", authorization).
+                header("Cookie", cookies).
+                when().
+                put(put_url).
+                then().
+                extract().
+                response();
+
+        Assert.assertEquals(response_put.getStatusCode(),200);
+        System.out.println("Body after put: " + response_put.getBody().asString());
+    }
+
+    @Test
+    public void postAndPatchBooking() {
+        String url = urlBooking();
+        String contentType = "application/json";
+
+        String bodyData = printJsonBooking();
+
+        Response response = RestAssured.
+                given().
+                contentType(contentType).
+                body(bodyData).
+                when().
+                post(url).
+                then().
+                extract().
+                response();
+
+        Assert.assertEquals(response.getStatusCode(), 200);
+        System.out.println("Body :" + response.getBody().asString());
+        int bookingid = response.jsonPath().getInt("bookingid");
+        System.out.println("bookingid: "  + bookingid);
+
+        String url2 =  urlBooking() + "/" + bookingid;
+        String cookies = cookies();
+        String authorization = authorization();
+        String bodyDataPatch = printJsonPatchbooking();
+
+        Response response_patch = RestAssured.
+                given().
+                contentType(contentType).
+                body(bodyDataPatch).
+                cookie(cookies).
+                header("Authorization", authorization).
+                header("Cookie", cookies).
+                when().
+                patch(url2).
+                then().
+                extract().
+                response();
+
+        Assert.assertEquals(response_patch.getStatusCode(),200);
+        System.out.println("status: " + response_patch.getBody().asString());
+    }
+
+    @Test
+    public void postAndDeleteBooking() {
+        String url = urlBooking();
+        String contentType = "application/json";
+        String bodyData = printJsonBooking();
+
+        Response response = RestAssured.
+                given().
+                contentType(contentType).
+                body(bodyData).
+                when().
+                post(url).
+                then().
+                extract().
+                response();
+
+        Assert.assertEquals(response.getStatusCode(), 200);
+//        System.out.println("Body :" + response.getBody().asString());
+        int bookingid = response.jsonPath().getInt("bookingid");
+        System.out.println("bookingid: "  + bookingid);
+
+        String url2 = urlBooking() + "/" + bookingid;
+        String cookies = cookies();
+        String authorization = authorization();
+
+        Response response2 = RestAssured.
+                given().
+                contentType(contentType).
+                cookie(cookies).
+                header("Authorization", authorization).
+                header("Cookie", cookies).
+                when().
+                delete(url2).
+                then().
+                extract().
+                response();
+
+        Assert.assertEquals(response2.getStatusCode(),201);
+        System.out.println("status: " + response2.getBody().asString());
+    }
+
+    @Test
+    public void postAndGetBookingById() {
+        String url = urlBooking();
+        String contentType = "application/json";
+        String bodyData = printJsonBooking();
+
+        Response response_post = RestAssured.
+                given().
+                contentType(contentType).
+                body(bodyData).
+                when().
+                post(url).
+                then().
+                extract().
+                response();
+
+        Assert.assertEquals(response_post.getStatusCode(), 200);
+        System.out.println("Body :" + response_post.getBody().asString());
+        int bookingid = response_post.jsonPath().getInt("bookingid");
+        System.out.println("bookingid: "  + bookingid);
+
+        String url_get = urlBooking() + "/" + bookingid;
+        Response response = RestAssured.
+                given().
+                get(url_get).
+                then().
+                statusCode(200).
+                extract().
+                response();
+
+        Assert.assertEquals(response.getStatusCode(),200);
+        System.out.println("Body after post: " + response.getBody().asString());
+    }
+
+    @Test
+    public void getBookingsReturnsBadUrl() {
+        Response response = get("https://restful-booker.herokuapp.com/bookings");
+
+        System.out.println("Response :" + response.asString());
+        System.out.println("status code: " + response.getStatusCode());
+        System.out.println("Body :" + response.getBody().asString());
+
+        int statusCode = response.getStatusCode();
+        Assert.assertEquals(statusCode, 404);
+    }
+    @Test
+    public void getBookingsReturnsBadMethod() {
+        Response response = post(urlBooking());
+
+        System.out.println("status code: " + response.getStatusCode());
+        System.out.println("Body :" + response.getBody().asString());
+
+        int statusCode = response.getStatusCode();
+        Assert.assertEquals(statusCode, 415);
+    }
+
+    @Test
+    public void postBookingBadBoolean() {
+        String url = "https://restful-booker.herokuapp.com/booking";
+        String contentType = "application/json";
+        String bodyData = printBadJsonBoolean();
+
+        Response response = RestAssured.
+                given().
+                contentType(contentType).
+                body(bodyData).
+                when().
+                post(url).
+                then().
+                extract().
+                response();
+
+        Assert.assertEquals(response.getStatusCode(),400);
+        System.out.println("Body :" + response.getBody().asString());
+    }
+
+    @Test
+    public void postBookingBadInt() {
+        String url = "https://restful-booker.herokuapp.com/booking";
+        String contentType = "application/json";
+        String bodyData = printBadJsonStringInt();
+
+        Response response = RestAssured.
+                given().
+                contentType(contentType).
+                body(bodyData).
+                when().
+                post(url).
+                then().
+                extract().
+                response();
+
+        Assert.assertEquals(response.getStatusCode(),400);
+        System.out.println("Body :" + response.getBody().asString());
+    }
+
+    @Test
+    public void postBookingBadMethodDel() {
+        String url = urlBooking();
+        String contentType = "application/json";
+        String bodyData = printJsonBooking();
+
+        Response response = RestAssured.
+                given().
+                contentType(contentType).
+                body(bodyData).
+                when().
+                delete(url).
+                then().
+                extract().
+                response();
+
+        Assert.assertEquals(response.getStatusCode(),404);
+        System.out.println("Body :" + response.getBody().asString());
+    }
+}
+
--- src/test/java/Data.java
+++ src/test/java/Data.java
@@ -4,6 +4,14 @@ public class Data extends Faker{
         return "https://restful-booker.herokuapp.com/booking";
     }
 
+    public static String cookies() {
+        return "token=f61416de503d436";
+    }
+
+    public static String authorization() {
+        return "Basic YWRtaW46cGFzc3dvcmQxMjM=";
+    }
+
     public static String printJsonBooking() {
         String names = printFirstNameNorway();
         String lnames = printLastNameNorway();
@@ -24,6 +32,26 @@ public class Data extends Faker{
                 "}";
     }
 
+    public static String printJsonPutBooking() {
+        String names = printFirstNameNorway();
+        String lnames = printLastNameNorway();
+        String needs = printNeeds();
+        int generator = printGenerator();
+        String date = printDate();
+        String datetomorrow = String.valueOf(printTomorrow());
+
+        return "{" +
+                " \"firstname\": " +  '"' + names + "up" +'"' + "," +
+                " \"lastname\": " +  '"' + lnames + '"' + "," +
+                " \"totalprice\": "  + generator + "," +
+                " \"depositpaid\": \"true\"," +
+                " \"bookingdates\":"  +
+                " { \"checkin\": " + '"' + date + '"'+ "," +
+                " \"checkout\": " + '"' + datetomorrow + '"' + "}," +
+                " \"additionalneeds\": " + '"' + needs + '"' +
+                "}";
+    }
+
     public static String printJsonPatchbooking() {
         String names = printFirstNameNorway();
         String lnames = printLastNameNorway();
@@ -32,6 +60,32 @@ public class Data extends Faker{
                 " \"firstname\": " + '"' + names + '"' + ","+
                 " \"lastname\": " +  '"' + lnames + '"' +
                 "}";
-
     }
+
+    public static String printBadJsonStringInt() {
+        return "{" +
+                " \"firstname\":  \"John\"," +
+                " \"lastname\":  \"Badminton\"," +
+                " \"totalprice\":  \"true\"," +
+                " \"depositpaid\": \"true\"," +
+                " \"bookingdates\":"  +
+                " { \"checkin\":  \"2022-01-14\"," +
+                " \"checkout\":  \"2022-01-15\"," +
+                " \"additionalneeds\":  \"Lunch\"," +
+                "}";
+    }
+
+    public static String printBadJsonBoolean() {
+        return "{" +
+                " \"firstname\":  \"John\"," +
+                " \"lastname\":  \"Badminton\"," +
+                " \"totalprice\":  \"true\"," +
+                " \"depositpaid\": \"bug\"," +
+                " \"bookingdates\":"  +
+                " { \"checkin\":  \"2022-01-14\"," +
+                " \"checkout\":  \"2022-01-15\"," +
+                " \"additionalneeds\":  \"Lunch\"," +
+                "}";
+    }
+
 }
