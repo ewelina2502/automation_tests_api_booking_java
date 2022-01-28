f4048870bf9d678555a8fddbaf14cc875b192402
ewelina2502 <piotrowska.ewelina@gmail.com>
2022-01-28 11:26:07 +0100

    Add new object without Faker generator

--- src/test/java/BookingObject.java
+++ src/test/java/BookingObject.java
@@ -3,6 +3,9 @@ public class BookingObject extends Faker {
 
     public static void main(String[] args) {
 
+        Booking first_booking = new Booking("Adam", "Kowalski", 123, true, "2022-01-28", "2022-01-28", "Breakfast" );
+        first_booking.info();
+
         Booking second_booking = new Booking(printFirstNameFaker(), printLastNameFaker(), printGenerator(), true, printDate(), printTomorrow(), printNeeds());
         second_booking.info();
 
