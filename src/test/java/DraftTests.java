5a2ea2ad62c424f931d124416ee798b8f6e9e47d
Ewelina <ewelina2502@gmail.com>
2022-02-10 18:05:58 +0100

    add loop with numbers of chars with refactor

--- src/test/java/DraftTests.java
+++ src/test/java/DraftTests.java
@@ -1,19 +1,26 @@
+import java.util.Random;
+
 public class DraftTests extends Data {
 
+    public static String printNeeds() {
+        String[] needs = {"Breakfast", "Lunch", "Dinner"};
+        return needs[new Random().nextInt(needs.length)];
+    }
 
     public static void main(String[] args) {
-        String needs = "Lunches";
-        int l = needs.length();
-//        char chars;
-//        System.out.println(l);
+
+        String[] needs = {"Breakfast", "Lunch", "Dinner", "Supper", "Delicious", "Fish", "Sandwich"};
+        int l = needs[new Random().nextInt(needs.length)].length();
+//        System.out.println(printNeeds());
+
 
         for (int i = 0; i < 1; i++) {
-//            chars = needs.charAt(i);
-
             if (l == 5 ) {
-                System.out.println(needs + " has 5 chars");
-            } else if (l > 10) {
-                System.out.println(needs + " has "+ l + " chars");
+                System.out.println("5 chars it must be Lunch");
+            } else if (l == 4) {
+                System.out.println("4 chars it must be Fish");
+            } else if (l >= 9) {
+                System.out.println(printNeeds() + " has more than 5 chars");
             } else {
                 System.out.println("It isn't 5 chars");
             }
