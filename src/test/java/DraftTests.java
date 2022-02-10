6ec39576bf564ee4b92811b63198dc1518e77043
Ewelina <ewelina2502@gmail.com>
2022-02-10 18:51:53 +0100

    add new else if and expected return

--- src/test/java/DraftTests.java
+++ src/test/java/DraftTests.java
@@ -2,27 +2,25 @@ import java.util.Random;
 
 public class DraftTests extends Data {
 
-    public static String printNeeds() {
-        String[] needs = {"Breakfast", "Lunch", "Dinner"};
-        return needs[new Random().nextInt(needs.length)];
-    }
 
     public static void main(String[] args) {
 
-        String[] needs = {"Breakfast", "Lunch", "Dinner", "Supper", "Delicious", "Fish", "Sandwich"};
-        int l = needs[new Random().nextInt(needs.length)].length();
-//        System.out.println(printNeeds());
-
+        String[] meals = {"Breakfast", "Lunch", "Dinner", "Supper", "Delicious", "Fish", "Sandwich"};
+        String meal = meals[new Random().nextInt(meals.length)];
+//        System.out.println(meal.length() + " chars in " + meal);
+        int l = meal.length();
 
         for (int i = 0; i < 1; i++) {
             if (l == 5 ) {
-                System.out.println("5 chars it must be Lunch");
+                System.out.println(meal +" has 5 chars it must be Lunch");
             } else if (l == 4) {
-                System.out.println("4 chars it must be Fish");
+                System.out.println(meal + " has 4 chars it must be Fish");
+            } else if (l == 6) {
+                System.out.println(meal + " has 6 chars in is expected: Dinner or Supper");
             } else if (l >= 9) {
-                System.out.println(printNeeds() + " has more than 5 chars");
+                System.out.println(meal + " it is equal or more than 9 chars is expected: Breakfast or Delicious");
             } else {
-                System.out.println("It isn't 5 chars");
+                System.out.println(meal + " hasn't 5 chars it has: " + meal.length() + " chars and is expected: Sandwich");
             }
         }
     }
